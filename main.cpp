6bc19cc2905097199e89bc116416475c84bdfbd1
Felix_the_kit <mazyandya2005@tut.by>
2023-02-13 09:21:29 +0300

    Named

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
 using namespace std;
 int main(){
-cout << "Hello world! I am on main branch!" << endl;
+cout << "Hello world! I am on main branch! My name is not Misha!" << endl;
 }
