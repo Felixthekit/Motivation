7fea0e95a504cae4fb60976cd6819840611f8b0e
Felix_the_kit <mazyandya2005@tut.by>
2023-02-13 23:03:46 +0300

    united

--- main.cpp
+++ main.cpp
@@ -2,4 +2,7 @@
 using namespace std;
 int main(){
 cout << "Hello world! I am on main branch! I AM THE STORM THAT IS APROACHING! PROVOKING BLACK CLOUDS IN I-S-O-L-A-T-I-O-N!" << endl;
+cin.get();
+cin.get();
+return 0;
 }
