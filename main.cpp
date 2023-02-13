317c51f2817859a3fbcf29d0241dd8774e444f59
Felix_the_kit <mazyandya2005@tut.by>
2023-02-13 09:30:36 +0300

    rebased

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
 using namespace std;
 int main(){
-cout << "Hello world! I am on main branch! My name is not Misha!" << endl;
+cout << "Hello world! I am on main branch! I AM THE STORM THAT IS APROACHING! PROVOKING BLACK CLOUDS IN I-S-O-L-A-T-I-O-N!" << endl;
 }
